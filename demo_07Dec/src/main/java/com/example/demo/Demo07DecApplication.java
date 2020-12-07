f50ec6e9bdb533d826c7e36998320a61df7db9c8
leaddevops <leaddevops@gmail.com>
2020-12-07 13:00:01 +0530

    FirstCommit'

---
+++ demo_07Dec/src/main/java/com/example/demo/Demo07DecApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo07DecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo07DecApplication.class, args);
+	}
+
+}
